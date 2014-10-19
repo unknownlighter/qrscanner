b3590af0794090aab962bc2ea65b1a101bc0d2ee
LighteR <unknownlighter@gmail.com>
2014-10-19 23:35:27 +0400

    performance optimization

--- qrscanner.py
+++ qrscanner.py
@@ -3,14 +3,17 @@ from PIL import Image, ImageFilter
 from math import atan2, degrees
 
 class RecognizedImage(object):
-    def __init__(self, pil, symbol):
+    def __init__(self, pil, symbol, is_modified=False):
         self.pil = pil
         self.symbol = symbol
+        self.is_modified = is_modified
+    
+    def get_title_height(self):
+        return max([y for x,y in self.symbol.location])
         
     def crop_title_by_qr(self):
         w, h = self.pil.size
-        title_height = max([y for x,y in self.symbol.location])
-        return self.pil.crop((0, title_height, w, h))
+        return self.pil.crop((0, self.get_title_height(), w, h))
 
 class QRScannerError(Exception):
     pass
@@ -39,21 +42,25 @@ class QRScanner(object):
     def scan(self, image_path):
         pil = Image.open(image_path).convert('L')
         
-        symbol = None
         angle = 0
-        while not symbol and angle<=180:
+        symbol = self.recognize_from_pil(pil)
+        while not symbol and angle<=150:
+            angle += 30
             rotated_pil = self.rotate_image(pil, -angle)
             symbol = self.recognize_from_pil(rotated_pil)
-            angle += 30
             
         if not symbol:
             raise QRScannerError('could not recognize qr-code')
         
-        rotatation_angle = self.get_rotation_angle(symbol) - angle + 30
-        up_oriented_pil = self.rotate_image(pil, rotatation_angle)
-        symbol = self.recognize_from_pil(up_oriented_pil)
+        rotatation_angle = self.get_rotation_angle(symbol) - angle
+        up_oriented_pil = pil
+        is_modified = False
+        if abs(rotatation_angle) > 5:
+            up_oriented_pil = self.rotate_image(pil, rotatation_angle)
+            symbol = self.recognize_from_pil(up_oriented_pil)
+            is_modified = True
         
         if not symbol:
             raise QRScannerError('could not orient image')
         
-        return RecognizedImage(up_oriented_pil, symbol)
+        return RecognizedImage(up_oriented_pil, symbol, is_modified)
